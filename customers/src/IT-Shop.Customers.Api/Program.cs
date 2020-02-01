b50e1f47c57ded252cf6cb1dbbd53edccfae9659
Dean Milojevic <milojevicdean@gmail.com>
2020-02-01 10:27:48 +0100

    Adding basic structure for the Customers service

---
+++ customers/src/IT-Shop.Customers.Api/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace IT_Shop.Customers.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        // Setup a HTTP/2 endpoint without TLS.
+                        // https://go.microsoft.com/fwlink/?linkid=2099682
+                        options.ListenLocalhost(5000, o => o.Protocols =
+                            HttpProtocols.Http2);
+                    });
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ customers/src/IT-Shop.Customers.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IT_Shop.Customers.Core
+{
+    public class Class1
+    {
+    }
+}
