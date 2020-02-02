3602507429c9e16542f0c72e666605e529b1bea5
Dean Milojevic <milojevicdean@gmail.com>
2020-02-02 15:00:10 +0100

    Implementing the CustomersService, WIP

--- customers/src/IT-Shop.Customers.Api/Services/CustomersService.cs
+++ customers/src/IT-Shop.Customers.Api/Services/CustomersService.cs
@@ -1,29 +1,51 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Grpc.Core;
 using IT_Shop.Customers.Api.Generated;
+using IT_Shop.Customers.Core.Queries;
+using Mediator;
 using Microsoft.Extensions.Logging;
 
 namespace ITShop.Customers.Api.Services
 {
-    public class CustomersService : CustomersService.CustomersServiceBase
+    public class CustomersService : IT_Shop.Customers.Api.Generated.CustomersService.CustomersServiceBase
     {
         private readonly ILogger<CustomersService> _logger;
+        private readonly IDispatcher _dispatcher;
 
-        public CustomersService(ILogger<CustomersService> logger)
+        public CustomersService(ILogger<CustomersService> logger, IDispatcher dispatcher)
         {
             _logger = logger;
+            _dispatcher = dispatcher;
         }
 
-        public override Task<CustomerModel> Get(CustomerQuery request, ServerCallContext context)
+        public override async Task<CustomerModel> Get(CustomerQuery request, ServerCallContext context)
         {
             _logger.LogInformation("Get the customer @{request}", request);
-            return Task.FromResult(new CustomerModel());
+
+            var result = await _dispatcher.Send(new GetCustomer(Guid.Parse(request.Id)));
+
+            return new CustomerModel
+            {
+                Id = result.Id.ToString(),
+                Name = result.Name
+            };
         }
 
-        public override Task<CustomersModel> GetAll(BrowseCustomersQuery request, ServerCallContext context)
+        public override async Task<CustomersModel> GetAll(BrowseCustomersQuery request, ServerCallContext context)
         {
             _logger.LogInformation("Get the customers @{request}", request);
-            return Task.FromResult(new CustomersModel());
+
+            var result = await _dispatcher.Send(new BrowseCustomers());
+
+            var responseModel = new CustomersModel();
+
+            foreach (var customer in result)
+            {
+                responseModel.Customers.Add(new CustomerModel { Id = customer.Id.ToString(), Name = customer.Name });
+            }
+
+            return responseModel;
         }
     }
 }
