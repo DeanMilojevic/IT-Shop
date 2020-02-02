54c04abd32cecca444dc0d12699de3f19b8b102e
Dean Milojevic <milojevicdean@gmail.com>
2020-02-02 12:17:05 +0100

    Adding registrations for the handlers

---
+++ customers/src/IT-Shop.Customers.Core.MongoDB/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using IT_Shop.Customers.Core.Models;
+using IT_Shop.Customers.Core.MongoDB.QueryHandlers;
+using IT_Shop.Customers.Core.Queries;
+using Mediator.AspNetCore.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IT_Shop.Customers.Core.MongoDB.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMongo(this IServiceCollection services)
+        {
+            services.AddQueryHandler<GetCustomer, Customer, GetCustomerHandler>();
+            services.AddQueryHandler<BrowseCustomers, IEnumerable<Customer>, BrowsseCustomersHandler>();
+
+            return services;
+        }
+    }
+}
