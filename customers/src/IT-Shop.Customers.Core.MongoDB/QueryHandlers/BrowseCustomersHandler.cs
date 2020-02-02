331019b770be2faaa290d3fc99627844d0a98c85
Dean Milojevic <milojevicdean@gmail.com>
2020-02-02 15:46:38 +0100

    Fixed a type

---
+++ customers/src/IT-Shop.Customers.Core.MongoDB/QueryHandlers/BrowseCustomersHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IT_Shop.Customers.Core.Models;
+using IT_Shop.Customers.Core.Queries;
+using Mediator;
+
+namespace IT_Shop.Customers.Core.MongoDB.QueryHandlers
+{
+    public class BrowseCustomersHandler : IQueryHandler<BrowseCustomers, IEnumerable<Customer>>
+    {
+        public BrowseCustomersHandler()
+        {
+        }
+
+        public Task<IEnumerable<Customer>> Handle(BrowseCustomers query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
