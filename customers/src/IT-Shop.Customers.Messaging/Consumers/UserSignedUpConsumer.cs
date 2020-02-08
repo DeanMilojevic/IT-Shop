c81427b754d6e2835bb31f35197974495a7c1be0
Dean Milojevic <milojevicdean@gmail.com>
2020-02-08 15:03:35 +0100

    Added consumer for the event when user signed up

---
+++ customers/src/IT-Shop.Customers.Messaging/Consumers/UserSignedUpConsumer.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using IT_Shop.Customers.Events;
+using MassTransit;
+
+namespace IT_Shop.Customers.Messaging.Consumers
+{
+    public class UserSignedUpConsumer : IConsumer<SignedUp>
+    {
+        public Task Consume(ConsumeContext<SignedUp> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ customers/src/IT-Shop.Customers.Messaging/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using GreenPipes;
+using IT_Shop.Customers.Messaging.Consumers;
+using MassTransit;
+using MassTransit.AspNetCoreIntegration;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IT_Shop.Customers.Messaging.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMessaging(this IServiceCollection services)
+        {
+            IBusControl CreateBus(IServiceProvider serviceProvider)
+            {
+                return Bus.Factory.CreateUsingRabbitMq(configurator =>
+                {
+                    configurator.Host("rabbitmq://localhost"); // todo config
+
+                    configurator.ReceiveEndpoint("user-signed-up", endpoint =>
+                    {
+                        endpoint.PrefetchCount = 16;
+                        endpoint.UseMessageRetry(retry => retry.Interval(2, 100));
+
+                        endpoint.ConfigureConsumer<UserSignedUpConsumer>(serviceProvider);
+                    });
+                });
+            }
+
+            static void ConfigureMassTransit(IServiceCollectionConfigurator configurator)
+            {
+                configurator.AddConsumer<UserSignedUpConsumer>();
+            }
+
+            services.AddMassTransit(CreateBus, ConfigureMassTransit);
+
+            return services;
+        }
+    }
+}
