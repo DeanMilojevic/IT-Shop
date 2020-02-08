9e40e8b1060fe2cd57f3505ab0b7f750f29963ad
Dean Milojevic <milojevicdean@gmail.com>
2020-02-08 15:46:37 +0100

    Added configuration class for the messaging

--- customers/src/IT-Shop.Customers.Messaging/Extensions/ServiceCollectionExtensions.cs
+++ customers/src/IT-Shop.Customers.Messaging/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using GreenPipes;
 using IT_Shop.Customers.Messaging.Consumers;
+using IT_Shop.Customers.Messaging.Options;
 using MassTransit;
 using MassTransit.AspNetCoreIntegration;
 using MassTransit.ExtensionsDependencyInjectionIntegration;
@@ -10,32 +11,32 @@ namespace IT_Shop.Customers.Messaging.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddMessaging(this IServiceCollection services)
+        public static IServiceCollection AddMessaging(this IServiceCollection services, MessagingOptions messagingOptions)
         {
-            IBusControl CreateBus(IServiceProvider serviceProvider)
-            {
-                return Bus.Factory.CreateUsingRabbitMq(configurator =>
-                {
-                    configurator.Host("rabbitmq://localhost"); // todo config
-
-                    configurator.ReceiveEndpoint("user-signed-up", endpoint =>
-                    {
-                        endpoint.PrefetchCount = 16;
-                        endpoint.UseMessageRetry(retry => retry.Interval(2, 100));
-
-                        endpoint.ConfigureConsumer<UserSignedUpConsumer>(serviceProvider);
-                    });
-                });
-            }
-
-            static void ConfigureMassTransit(IServiceCollectionConfigurator configurator)
-            {
-                configurator.AddConsumer<UserSignedUpConsumer>();
-            }
-
-            services.AddMassTransit(CreateBus, ConfigureMassTransit);
+            services.AddMassTransit(provider => CreateBus(provider, messagingOptions), ConfigureMassTransit);
 
             return services;
+        }
+
+        private static IBusControl CreateBus(IServiceProvider serviceProvider, MessagingOptions messagingOptions)
+        {
+            return Bus.Factory.CreateUsingRabbitMq(configurator =>
+            {
+                configurator.Host(messagingOptions.Host);
+
+                configurator.ReceiveEndpoint("user-signed-up", endpoint =>
+                {
+                    endpoint.PrefetchCount = 1;
+                    endpoint.UseMessageRetry(retry => retry.Interval(2, 100));
+
+                    endpoint.ConfigureConsumer<UserSignedUpConsumer>(serviceProvider);
+                });
+            });
+        }
+
+        private static void ConfigureMassTransit(IServiceCollectionConfigurator configurator)
+        {
+            configurator.AddConsumer<UserSignedUpConsumer>();
         }
     }
 }
---
+++ customers/src/IT-Shop.Customers.Messaging/Options/MessagingOptions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IT_Shop.Customers.Messaging.Options
+{
+    public class MessagingOptions
+    {
+        public MessagingOptions()
+        {
+            [Required]
+            public string Host { get; set; }
+    }
+    }
+}
