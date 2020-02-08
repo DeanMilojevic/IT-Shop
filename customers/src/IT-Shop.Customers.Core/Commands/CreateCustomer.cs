132a0784e30f78d5f9e9219e3e5d24c36eaa137e
Dean Milojevic <milojevicdean@gmail.com>
2020-02-08 20:51:31 +0100

    Added commands for creating customer and updating the billing details

---
+++ customers/src/IT-Shop.Customers.Core/Commands/CreateCustomer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace IT_Shop.Customers.Core.Commands
+{
+    public class CreateCustomer
+    {
+        public CreateCustomer(
+            Guid id,
+            string billingZipCode, string billingStreet, string billingNumber,
+            string shippingZipCode, string shippingStreet, string shippingNumber)
+        {
+            Id = id;
+            BillingZipCode = billingZipCode;
+            BillingStreet = billingStreet;
+            BillingNumber = billingNumber;
+            ShippingZipCode = shippingZipCode;
+            ShippingStreet = shippingStreet;
+            ShippingNumber = shippingNumber;
+        }
+
+        public Guid Id { get; }
+        public string BillingZipCode { get; }
+        public string BillingStreet { get; }
+        public string BillingNumber { get; }
+        public string ShippingZipCode { get; }
+        public string ShippingStreet { get; }
+        public string ShippingNumber { get; }
+    }
+}
---
+++ customers/src/IT-Shop.Customers.Core/Commands/UpdateCustomersShippingBillingDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace IT_Shop.Customers.Core.Commands
+{
+    public class UpdateCustomersShippingBillingDetails
+    {
+        public UpdateCustomersShippingBillingDetails(
+            Guid customerId,
+            string zipCode, string street, string number)
+        {
+            CustomerId = customerId;
+            ZipCode = zipCode;
+            Street = street;
+            Number = number;
+        }
+
+        public Guid CustomerId { get; }
+        public string ZipCode { get; }
+        public string Street { get; }
+        public string Number { get; }
+    }
+}
